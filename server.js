7f74a087cbc415520aeb7b1058cacd15c01cad5a
jdwoj5 <53838167+jdwoj5@users.noreply.github.com>
2020-06-28 11:51:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ require('dotenv').config();
 const express = require('express');
 const Discord = require('discord.js');
 
-
-
 const bot = new Discord.Client({
     presence: {
         status: 'dnd',
@@ -19,13 +17,7 @@ const bot = new Discord.Client({
     }
 });
 bot.on('ready', async () => {
-    console.log('DISCORD | Online!')
-    process.on('beforeExit', (code) => {
-        client.setPresence({
-            status: 'offline'
-        })
-        console.log('DISCORD | Offline!');
-    });
+    console.log('DISCORD | Online!');
 });
 bot.on('error', async (error) => {
     console.log(`DISCORD | Error ${error.name}: ${error.message} \n(File: ${error.fileName}, Line: ${error.lineNumber})\n${error.stack}`)
@@ -39,7 +31,34 @@ const app = express();
 app.get('/', async (request, response) => {
     response.status(200);
     response.json({ status: 'ok', running: true })
-})
+});
 const listener = app.listen(process.env.HUB_ACCESSPORT || 8080, async () => {
-    console.log('WEB | Online!')
-})
+    console.log('WEB | Online!');
+});
+
+process.stdin.resume();
+
+function exitHandler(options, exitCode) {
+    if (bot.user) {
+        bot.user.setPresence({
+            status: 'offline'
+        });
+        bot.destroy();
+    }
+    process.exit();
+}
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+
+process.on('beforeExit', (code) => {
+    bot.setPresence({
+        status: 'offline'
+    });
+    bot.destroy();
+    console.log('DISCORD | Offline!');
+    app.close();
+    console.log('WEB | Offline!')
+});
