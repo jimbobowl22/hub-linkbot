0c81afffcc9e4c8ef906a8cf1a22d70e600687c0
jdwoj5 <53838167+jdwoj5@users.noreply.github.com>
2020-07-06 15:56:07 -0400

    Help Upgrades

--- commands/help.js
+++ commands/help.js
@@ -18,7 +18,27 @@ module.exports = {
             .setFooter('This bot has been written by jdwoj5butbetter#1132.')
             .setThumbnail(guild.iconURL())
             // .addField('Description', 'This bot is used to deliver products to users who buy them. It is also used for account linking and to get user information.', true)
-        bot.commands.array().map(command => ThisEmbed.addField(process.env.BOT_PREFIX+command.name+command.arguments.map(a => ` \`${a.label}\``), `*${command.description}*`, true))
+        bot.commands.array().map(command => {
+            if (message.guild) {
+                let upermission = message.member.permissionsIn(message.channel)
+                var matching = []
+                for (permission of command.userPermissions) {
+                    if (!upermission.has(permission, true)) matching.push('`'+permission+'`')
+                }
+                if (matching.length > 0) {
+                    return
+                }
+                let mepermission = message.guild.me.permissionsIn(message.channel)
+                var matching = []
+                for (permission of command.clientPermissions) {
+                    if (!mepermission.has(permission, true)) matching.push('`'+permission+'`')
+                }
+                if (matching.length > 0) {
+                    return
+                }
+            }
+            ThisEmbed.addField(process.env.BOT_PREFIX+command.name+command.arguments.map(a => ` \`${a.label}\``), `*${command.description}*`, true)
+        })
         await message.channel.send(ThisEmbed)
 	}
 };
